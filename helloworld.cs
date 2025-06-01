87c6988c375c2828cd8118f343c882a243518e91
moonie232 <moonfan2009@gmail.com>
2025-06-01 20:53:20 -0300

    Add files via upload

---
+++ helloworld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        atic void Main(string[] args)
+        {
+            Console.WriteLine("Ola, Mundo!, Konnichiwa, Sekai!, Hello, World!");
+            Console.WriteLine("esse é o meu primeiro projeto em C#.");
+        }
+    }
+}
